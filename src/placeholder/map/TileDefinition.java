3fd5d9abf29713f625e81deff0564ec15df7a603
russell <russell@russell-HP-HDX-16-Notebook-PC>
2013-10-27 23:03:30 -0500

    * Added lwjgl * Expanded Tile defintions

--- src/placeholder/map/TileDefinition.java
+++ src/placeholder/map/TileDefinition.java
@@ -1,5 +1,7 @@
 package placeholder.map;
 
+import placeholder.render.TileRenderer;
+
 /**
  *
  * @author russell
@@ -8,11 +10,12 @@ public class TileDefinition {
   public final String name;
   public final String canonname;
   public final byte metadata;
+  public final boolean opaque;
 
-  public TileDefinition(String name, String canonname, byte metadata) {
-    assert metadata <= 48;
+  public TileDefinition(String name, String canonname, byte metadata, boolean opaque) {
     this.name = name;
     this.canonname = canonname;
     this.metadata = metadata;
+    this.opaque = opaque;
   }
 }
