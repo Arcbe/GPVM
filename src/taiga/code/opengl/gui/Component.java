d28b6b8169020d228d01d388b24bc984505409ae
russell <russell@russell-G75VW>
2014-10-11 06:12:15 -0500

    * Updated license headers

--- src/taiga/code/opengl/gui/Component.java
+++ src/taiga/code/opengl/gui/Component.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Russell Smith.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
  */
 
 package taiga.code.opengl.gui;
@@ -9,7 +22,6 @@ package taiga.code.opengl.gui;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.util.logging.Logger;
-import org.lwjgl.opengl.GL11;
 import taiga.code.math.Matrix4;
 import taiga.code.math.ReadableMatrix4;
 import taiga.code.opengl.Renderable;
--- src/taiga/code/registration/MissingObjectException.java
+++ src/taiga/code/registration/MissingObjectException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Russell Smith, All rights reserved.
+ * Copyright (C) 2014 Russell Smith.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -12,7 +12,9 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
  */
 
 package taiga.code.registration;
